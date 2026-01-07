d23fca3a9e36261217a7bedecabc0cbe26eb435b
adisdee <adityaa.sde@gmail.com>
2026-01-07 21:53:09 +0530

    include consume token function and bucket refilling function

--- java/src/main/core/tokenBucket.java
+++ java/src/main/core/tokenBucket.java
@@ -5,6 +5,7 @@ import main.time.systemTimeProvider;
 public class tokenBucket {
     private final int bucketCapcity;
     private int tokens;
+    private final long refillInterval;
     private long lastRefillTime;
     private final systemTimeProvider timeProvider;
 
@@ -13,13 +14,35 @@ public class tokenBucket {
         this.tokens = bucketCapcity;
         this.lastRefillTime = timeProvider.nowMillis();
         this.timeProvider = timeProvider;
-    }
+        this.refillInterval = refillInterval;
+    };
 
-    public int getTokens() {
-        return tokens;
-    }
+    public boolean consumeToken() {
+        // checks the last refill time of bucket and compare between refillinterval
+        // if last refill time is greater than refillinterval
 
+        refillBucketIfNeeded();
+
+        if (tokens < 0)
+            return false;
+        tokens--;
+        return true;
+    };
+
+    public void refillBucketIfNeeded() {
+        // comparison between current time and last refill time
+
+        long now = timeProvider.nowMillis();
+        long elapsed = now - lastRefillTime;
+
+        if (elapsed >= refillInterval) {
+            tokens = bucketCapcity;
+            lastRefillTime = now;
+        }
+    };
+
+    @Override
     public String toString() {
-        return "token" + tokens + "," + "lastRefillTime" + lastRefillTime + "capcity" + bucketCapcity;
+        return "{ tokens" + tokens + "bucket" + bucketCapcity + " }";
     }
 }

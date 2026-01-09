11dda143face1fd2f7102b6103de6a1b07e24f8a
adisdee <adityaa.sde@gmail.com>
2026-01-09 21:34:05 +0530

    update constructor with boundr config and include cleanup buckets method

--- java/src/main/api/boundr.java
+++ java/src/main/api/boundr.java
@@ -1,13 +1,14 @@
 package main.api;
 
+import main.config.boundrConfig;
 import main.registry.bucketRegistry;
 import main.time.systemTimeProvider;
 
 public class boundr {
     private final bucketRegistry registry;
 
-    public boundr(int bucketCapcity, long refillInterval) {
-        this.registry = new bucketRegistry(bucketCapcity, refillInterval, new systemTimeProvider());
+    public boundr(boundrConfig config) {
+        this.registry = new bucketRegistry(config.getCapcity(), config.getRefillInterval(), new systemTimeProvider());
     }
 
     // needs to pass a key that will go in registry then goes to token bucket it
@@ -19,5 +20,11 @@ public class boundr {
 
     public int getTokens(String key) {
         return registry.getBucket(key).getTokens();
+    }
+
+    // clear buckets that are not used for last 5 minutes
+
+    public void cleanupBuckets() {
+        registry.cleanupUnusedBuckets();
     }
 }

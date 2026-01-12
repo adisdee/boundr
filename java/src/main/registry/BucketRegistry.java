242be90781ca4c05c777fbeaf32356f747e0d13f
adisdee <adityaa.sde@gmail.com>
2026-01-12 22:13:46 +0530

    update changes and method names

--- java/src/main/registry/BucketRegistry.java
+++ java/src/main/registry/BucketRegistry.java
@@ -2,47 +2,47 @@ package main.registry;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-import main.core.tokenBucket;
-import main.time.systemTimeProvider;
+import main.core.TokenBucket;
+import main.time.TimeProvider;
 
-public class bucketRegistry {
-    // a thread safe map that return correct bucket as per the key
+/*
+ * Registry for managing buckets per key
+ */
 
-    private final int bucketCapcity;
+public class BucketRegistry {
+
+    private final int bucketCapacity;
     private final long refillInterval;
-    private final systemTimeProvider timeProvider;
-    private static final long bucketTTL = 5 * 60 * 1000;
+    private final TimeProvider timeProvider;
+    private static final long BUCKET_TTL_MS = 5 * 60 * 1000;
 
-    private final ConcurrentHashMap<String, tokenBucket> buckets = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, TokenBucket> buckets = new ConcurrentHashMap<>();
 
-    // configs
-
-    public bucketRegistry(int bucketCapcity, long refillInterval, systemTimeProvider timeProvider) {
-        this.bucketCapcity = bucketCapcity;
+    public BucketRegistry(int bucketCapacity, long refillInterval, TimeProvider timeProvider) {
+        this.bucketCapacity = bucketCapacity;
         this.refillInterval = refillInterval;
         this.timeProvider = timeProvider;
     }
 
-    // get and creation of bucket
+    /*
+     * Returns existing bucket for the key or creates new one
+     */
 
-    public tokenBucket getBucket(String key) {
+    public TokenBucket getBucket(String key) {
         return buckets.computeIfAbsent(key,
-                k -> new tokenBucket(bucketCapcity, refillInterval, timeProvider));
+                k -> new TokenBucket(bucketCapacity, refillInterval, timeProvider));
     }
 
-    public void showBuckets() {
-        buckets.forEach((key, bucket) -> System.out.println(key + "->" + bucket));
-    }
-
-    // unused buckets cleanup
+    /*
+     * Remove unused buckets that are not accessed within TTL
+     */
 
     public void cleanupUnusedBuckets() {
-        long now = timeProvider.nowMillis();
+        long now = timeProvider.now();
 
         buckets.entrySet().removeIf(entry -> {
-            tokenBucket bucket = entry.getValue();
-            return now - bucket.getLastAccessTime() > bucketTTL;
+            TokenBucket bucket = entry.getValue();
+            return now - bucket.getLastAccessTime() > BUCKET_TTL_MS;
         });
     }
-
 }

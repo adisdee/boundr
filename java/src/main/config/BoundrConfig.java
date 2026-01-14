cb37c0e7dba6a9c1421cff85b67ae761325ec00e
adisdee <adityaa.sde@gmail.com>
2026-01-14 21:28:20 +0530

    fix method name conflicts and include new changes

--- java/src/main/config/BoundrConfig.java
+++ java/src/main/config/BoundrConfig.java
@@ -1,18 +1,33 @@
 package main.config;
 
+/* Configuration for boundr rate limiter */
+
 public class BoundrConfig {
-    private final int bucketCapcity;
+    private final int bucketCapacity;
     private final long refillInterval;
 
-    public BoundrConfig(int bucketCapcity, long refillInterval) {
-        this.bucketCapcity = bucketCapcity;
+    public BoundrConfig(int bucketCapacity, long refillInterval) {
+
+        if (bucketCapacity <= 0) {
+            throw new IllegalArgumentException("Bucket capacity must be greater than 0");
+        }
+
+        if (refillInterval <= 0) {
+            throw new IllegalArgumentException("Refill Interval must be greater than 0");
+        }
+
+        this.bucketCapacity = bucketCapacity;
         this.refillInterval = refillInterval;
     }
 
-    public int getCapcity() {
-        return bucketCapcity;
+    /* @return bucket capcity */
+
+    public int getCapacity() {
+        return bucketCapacity;
     }
 
+    /* @return bucket refill interval */
+    
     public long getRefillInterval() {
         return refillInterval;
     }
